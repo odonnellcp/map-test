37c6b4a43d077105cdc0dd43e78889f8678ac312
odonnellcp <connor.odonnell625@gmail.com>
2018-05-06 15:04:01 -0700

    Updated Lat/Long Marker

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,28 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  updateCheck: '',
+  locCoordinates: '',
+  locGive: '',
+  locName: '',
   latitude: '',
   longitude: '',
-  lat: 45.519743,
-  lng: -122.680522,
-  zoom: 10,
+  lat: 33.876118,
+  lng: -117.921410,
+  zoom: 13,
 
   actions: {
     updateMap(){
       this.set('lat', this.get('latitude'));
       this.set('lng', this.get('longitude'));
+      this.set('locName', this.get('locGive'));
+      this.set('locGive', '');
       this.set('latitude', '');
       this.set('longitude', '');
+      this.set('updateCheck', 'true');
+      this.set('locCoordinates', [this.get('lat'), this.get('lng')]);
+      this.set('zoom', 20);
+      this.set
     }
   }
 });
