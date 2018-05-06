57f300e2478da1ed5d29d2272cf3bef4151a5571
odonnellcp <connor.odonnell625@gmail.com>
2018-05-06 13:42:38 -0700

    Added Longitude/Latitude Form

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,18 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  latitude: '',
+  longitude: '',
   lat: 45.519743,
   lng: -122.680522,
-  zoom: 10
+  zoom: 10,
+
+  actions: {
+    updateMap(){
+      this.set('lat', this.get('latitude'));
+      this.set('lng', this.get('longitude'));
+      this.set('latitude', '');
+      this.set('longitude', '');
+    }
+  }
 });
